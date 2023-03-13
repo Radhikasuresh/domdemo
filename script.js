2f66561f78574d695ba49909bac17db742320ca6
Radhikasuresh <124970046+Radhikasuresh@users.noreply.github.com>
2023-03-13 12:15:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function foo(){
+    var res=document.getElementById("fname").value;
+console.log(res);
+var res1=document.getElementById("mname").value;
+console.log(res1);
+var res2=document.getElementById("lname").value;
+console.log(res2);
+var res3=document.getElementById("email").value;
+console.log(res3);
+var res4=document.getElementById("pass").value;
+console.log(res4);
+}
